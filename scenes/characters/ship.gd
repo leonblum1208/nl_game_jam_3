ee8ac93f5244c49126e1215831ce516efc192f49
gummangummangumman <gumman@gumman.one>
2025-04-11 22:18:02 +0200

    beautiful ship sprite

--- scenes/characters/ship.gd
+++ scenes/characters/ship.gd
@@ -5,6 +5,17 @@ extends CharacterBody2D
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
+	#TODO move animations out of input maybe
+	if (input_direction.y > 0):
+		$Sprite2D.texture = ResourceLoader.load("res://sprites/ship_down.png")
+	elif (input_direction.y < 0):
+		$Sprite2D.texture = ResourceLoader.load("res://sprites/ship_up.png")
+	elif (input_direction.x > 0):
+		$Sprite2D.texture = ResourceLoader.load("res://sprites/ship_side.png")
+		$Sprite2D.scale.x = -1
+	elif (input_direction.x < 0):
+		$Sprite2D.texture = ResourceLoader.load("res://sprites/ship_side.png")
+		$Sprite2D.scale.x = 1
 
 func _physics_process(delta):
 	get_input()
