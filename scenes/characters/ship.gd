327d2e0f1f0f64ef820c64dd01d1b72c1508e275
Leon Blum <82680668+leonblum1208@users.noreply.github.com>
2025-04-12 15:34:29 +0200

    turning ship sprite

--- scenes/characters/ship.gd
+++ scenes/characters/ship.gd
@@ -1,22 +1,32 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@onready var ship_0 = $ShipSprite
+@export var speed = 100
 
-func get_input():
-	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = input_direction * speed
-	#TODO consider moving sprite stuff out of get_input
-	if (input_direction.y > 0):
-		$Sprite2D.texture = ResourceLoader.load("res://sprites/ship_down.png")
-	elif (input_direction.y < 0):
-		$Sprite2D.texture = ResourceLoader.load("res://sprites/ship_up.png")
-	elif (input_direction.x > 0):
-		$Sprite2D.texture = ResourceLoader.load("res://sprites/ship_side.png")
-		$Sprite2D.scale.x = -1
-	elif (input_direction.x < 0):
-		$Sprite2D.texture = ResourceLoader.load("res://sprites/ship_side.png")
-		$Sprite2D.scale.x = 1
+@onready var sprite = $ShipSprite
+var boat_images : Array[Texture2D] = []
+
+func _ready():
+	for i in 16:
+		var tex = load("res://sprites/Ship/ship%d.png" % (i+1))
+		boat_images.append(tex)
 
 func _physics_process(_delta):
-	get_input()
+	var input_direction: Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	velocity = input_direction * speed
+	if velocity.length() > 0:
+		rotation = velocity.angle() + deg_to_rad(90)
+	select_sprite()
 	move_and_slide()
+
+func select_sprite():
+	var angle_deg = int(rad_to_deg(rotation)) % 360
+	# Rotate reference so 0° is up instead of right
+	var adjusted_angle = (angle_deg) % 360
+	# Flip angle to make it counterclockwise
+	adjusted_angle = (360 - adjusted_angle) % 360
+	var index = int(round(adjusted_angle / 22.5)) % 16
+	sprite.texture = boat_images[index]
+	# ignore parent rotation
+	sprite.global_rotation = 0
