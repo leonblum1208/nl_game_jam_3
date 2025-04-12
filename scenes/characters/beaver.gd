fea029e3b808a8eccafd2a890019efa19f05b67f
Laura K <laurackuper@gmail.com>
2025-04-12 23:07:27 +0200

    Add beaver and work on land2

---
+++ scenes/characters/beaver.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+const SPEED = 30
+
+var direction = 1
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if ray_cast_2d.is_colliding():
+		rotation += PI
+		
+	position += SPEED * delta * Vector2.RIGHT.rotated(rotation) 
