bc28f23e85147164382070c8d2f51ab700fda881
Leon Blum <82680668+leonblum1208@users.noreply.github.com>
2025-04-12 23:17:03 +0200

    fix beavers getting stuck

--- scenes/characters/beaver.gd
+++ scenes/characters/beaver.gd
@@ -7,8 +7,7 @@ var direction = 1
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if ray_cast_2d.is_colliding():
 		rotation += PI
-		
 	position += SPEED * delta * Vector2.RIGHT.rotated(rotation) 
